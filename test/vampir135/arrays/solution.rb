183ec9604471fb47f7b51afb65fa6fd2f5787633
anton_tereshkin <tereshkin_94@mail.ru>
2016-06-02 15:15:38 +0400

    fix style code

--- test/vampir135/arrays/solution.rb
+++ test/vampir135/arrays/solution.rb
@@ -3,24 +3,23 @@ module Vampir135
     class << self
       def replace(array)
         max_element = array.max
-        array.map { |elem| (elem > 0 ? max_element : elem )   }
+        array.map { |elem| elem > 0 ? max_element : elem }
       end
 
       def search(array, query)
         array = array.sort
-        return -1 if array.size == 0
+        return -1 if array.empty?
         left = array.index(array.min)
         right = array.index(array.max)
         binary_search_array(array, query, left, right)
       end
 
       def binary_search_array(array, query, left, right)
-        mid = left + (right - left)/2
+        mid = left + (right - left) / 2
         return mid if query == array[mid]
         return -1 if left == right
-        if(query < array[mid] ? right = mid : left = mid + 1)
-          binary_search_array(array, query, left, right)
-        end
+        query < array[mid] ? right = mid : left = mid + 1
+        binary_search_array(array, query, left, right)
       end
     end
   end
